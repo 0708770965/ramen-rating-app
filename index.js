eca81c9e52c6bd9530ec8e9fc3a88ffd931c2bf9
hermann <hermanike131@gmail.com>
2025-03-17 10:23:13 +0300

    index.html

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const ramens = [
     { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "miso.jpg", rating: 4, comment: "Very flavorful!" },
     { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "tonkotsu.jpg" }
  ];
+ const ramens = [
+    {image_url: 'ramen1.jpg',
+        name: 'Tonkotsu Ramen',
+        restaurant: 'Ramen-ya',
+        rating: 4.5,
+        comment: 'Delicious!'},
+
+ ]
